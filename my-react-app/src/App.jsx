80cad85a17f3a4677a3250c4d7a4f1f4cd56d626
place-holder1 <138268286+place-holder1@users.noreply.github.com>
2025-01-28 17:27:41 -0500

    lab 5
    lab 5

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -16,12 +16,68 @@ const App = () => {
       name: 'John Doe',
       title: 'Software Engineer',
       email: 'a@a.com',
+    },
+    {
+      img: warm,
+      name: 'John Woe',
+      title: 'Software Developer',
+      email: 'c@a.com',
+    },
+    {
+      img: warm,
+      name: 'John Croe',
+      title: 'Software Bugger',
+      email: 'f@a.com',
+    },
+    {
+      img: warm,
+      name: 'John Yoe',
+      title: 'Software Weaponizer',
+      email: 'j@a.com',
+    },
+    {
+      img: warm,
+      name: 'John Toe',
+      title: 'Software Developer',
+      email: 'l@a.com',
     }
   ];
+  //get titles
+  const titles = [...new Set(profiles.map((profile) => profile.title))];
+
+
   const [count, setCount] = useState(0)
   const handleClick = ( ) => {
     console.log("Button clicked");
   };
+
+  const [title, setTitle] = useState("");
+  //update the title on change of the dropdown
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const [search, setSearch] = useState("");
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  }
+
+  const handleClear = () => {
+    setTitle("");
+    setSearch("");
+    console.log(setSearch());
+  }
+
+  const filteredProfiles = profiles.filter((profile) =>
+    // if(title === ""){
+    //   return true;
+    // }
+    // else{
+    //   return profile.title === title;
+    // }
+    (title === "" || profile.title === title && profile.name.toLowerCase().includes(search.toLowerCase()))
+  )
   return (
     <>
     <header>
@@ -36,10 +92,27 @@ const App = () => {
           <About />
        </Wrapper>
       <Wrapper>
+        <div className="filter-wrapper">
+          <div className="filter--select">
+            <label htmlFor="title-select">Select a title:</label>
+            <select id="title-select" onChange={handleTitleChange}>
+              {titles.map((title) => (<option key={title} value={title}>{title}</option>))}
+              {/* <option value="">All</option>
+              <option value="Software Engineer">Software Engineer</option>
+              <option value="Software Developer">Software Developer</option>
+              <option value="Software Bugger">Software Bugger</option>
+              <option value="Software Weaponizer">Software Weaponizer</option> */}
+            </select>
+            <input type="text" 
+              id="search"
+              onChange={handleSearchChange}
+              />
+            </div>
+            <button onClick={handleClear}>Clear</button>
+          </div>
           <div className="profile-cards">
-            {profiles.map((profile) => (
-              <Card key = {profile.email} {...profile} />
-          ))}
+            {/* {profiles.map((profile) => ( */}
+            {filteredProfiles.map((profile) => (<Card key={profile.email} {...profile} />))}
           </div>
         </Wrapper>
     </main>
